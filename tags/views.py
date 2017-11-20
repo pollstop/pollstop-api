fd078e68038c4fe86cff73128c208dd59cc9696d
Omar Albeik <omaralbeik@gmail.com>
2017-11-20 20:58:34 +0300

    Add polls detail route

--- tags/views.py
+++ tags/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets, mixins
-from rest_framework.decorators import list_route
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from . import models, serializers
+import polls
 
 
 class TagViewSet(mixins.ListModelMixin,
@@ -15,7 +17,9 @@ class TagViewSet(mixins.ListModelMixin,
 
     # list route to return polls for given tag
     # .../tags/<tag_id>/polls
-    @list_route()
-    def polls(self, request):
-        # example: https://github.com/omaralbeik/omaralbeik.com-api/blob/master/tags/views.py#L44
-        pass
+    @detail_route()
+    def polls(self, request, pk=None):
+        tag = self.get_object()
+        questions = polls.models.Question.objects.all().filter(tags__in=[tag])
+        serializer = polls.serializers.PollSerializer(questions, many=True)
+        return Response(serializer.data)
