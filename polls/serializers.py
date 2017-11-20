f227edadc1957ab9a97fa9856a96127ee30b6464
Omar Albeik <omaralbeik@gmail.com>
2017-11-20 20:59:44 +0300

    Add choices to serializer

--- polls/serializers.py
+++ polls/serializers.py
@@ -7,6 +7,11 @@ class PollSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField()
     choices = serializers.SerializerMethodField()
 
+    choice_1 = serializers.CharField(write_only=True, default=None)
+    choice_2 = serializers.CharField(write_only=True, default=None)
+    choice_3 = serializers.CharField(write_only=True, default=None)
+    choice_4 = serializers.CharField(write_only=True, default=None)
+
     class Meta:
         fields = (
             'type',
@@ -16,9 +21,45 @@ class PollSerializer(serializers.ModelSerializer):
             'date_created',
             'owner',
             'choices',
-            'tags',
+            'choice_1',
+            'choice_2',
+            'choice_3',
+            'choice_4',
         )
+
         model = models.Question
+
+    def validate(self, attrs):
+        """
+        Check that the question has at least 2 choices
+        """
+        choices = [
+            attrs['choice_1'],
+            attrs['choice_2'],
+            attrs['choice_3'],
+            attrs['choice_4'],
+        ]
+        numberOfChoices = sum(x is not None for x in choices)
+
+        if numberOfChoices < 2:
+            raise serializers.ValidationError("Question must have at least 2 choices")
+        return attrs
+
+    def create(self, validated_data):
+        choice_1 = validated_data.pop('choice_1', None)
+        choice_2 = validated_data.pop('choice_2', None)
+        choice_3 = validated_data.pop('choice_3', None)
+        choice_4 = validated_data.pop('choice_4', None)
+
+        question = models.Question(**validated_data)
+        question.save()
+
+        for c in [choice_1, choice_2, choice_3, choice_4]:
+            if c:
+                choice = models.Choice(text=c, question=question)
+                choice.save()
+
+        return question
 
     def get_type(self, question):
         return 'poll'
@@ -29,12 +70,12 @@ class PollSerializer(serializers.ModelSerializer):
         serializer = ChoiceSerializer(choices, many=True)
         return serializer.data
 
-
 class ChoiceSerializer(serializers.ModelSerializer):
     votes = serializers.SerializerMethodField()
 
     class Meta:
         fields = (
+            'id',
             'text',
             'votes',
         )
